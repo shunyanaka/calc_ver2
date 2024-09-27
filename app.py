127ca95b858a3593fbb7d2c26d9e20f7888a4264
shunyanaka <shunyanakamuraschool@gmail.com>
2024-09-27 20:58:55 +0900

    正答率に応じて選択肢の個数を増加

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 answer = None
-coutinue_correct = 4
+coutinue_correct = 23
 num = 0
 
 @app.route('/')
@@ -35,33 +35,46 @@ def quiz():
         num = 0
         return render_template('start.html')
 
-    if coutinue_correct >= 18:
+    if coutinue_correct >= 21:
         random_integer1 = random.randint(10000, 99999)
         random_integer2 = random.randint(10000, 99999)
+        number_chioce = 20
+
+
+    elif coutinue_correct >= 18:
+        random_integer1 = random.randint(10000, 99999)
+        random_integer2 = random.randint(10000, 99999)
+        number_chioce = 16
 
     elif coutinue_correct >= 15:
         random_integer1 = random.randint(10000, 99999)
         random_integer2 = random.randint(1000, 9999)
+        number_chioce = 16
 
     elif coutinue_correct >= 12:
         random_integer1 = random.randint(1000, 9999)
         random_integer2 = random.randint(1000, 9999)
+        number_chioce = 12
 
     elif coutinue_correct >= 9:
         random_integer1 = random.randint(1000, 9999)
         random_integer2 = random.randint(100, 999)
+        number_chioce = 12
 
     elif coutinue_correct >= 6:
         random_integer1 = random.randint(100, 999)
         random_integer2 = random.randint(100, 999)
+        number_chioce = 8
 
     elif coutinue_correct >= 3:
         random_integer1 = random.randint(100, 999)
-        random_integer2 = random.randint(10, 99)
+        random_integer2 = random.randint(20, 99)
+        number_chioce = 8
 
     else:
-        random_integer1 = random.randint(10, 99)
-        random_integer2 = random.randint(10, 99)
+        random_integer1 = random.randint(20, 99)
+        random_integer2 = random.randint(20, 99)
+        number_chioce = 8
 
     factor = random.randint(1, 2)
 
@@ -71,7 +84,7 @@ def quiz():
     else:
         if random_integer1 < random_integer2:
             random_integer1, random_integer2 = random_integer2, random_integer1
-        answer = random_integer1 - random_integer2
+        answer = random_integer1 - random_integer2 
 
     # 正解とランダムな間違いを含む選択肢を生成
         # 正解を含む選択肢を生成
@@ -82,7 +95,7 @@ def quiz():
     else:
         ten_answer = answer + 10
     choices.append(ten_answer)
-    while len(choices) < 8:
+    while len(choices) < number_chioce:
         fake_answer = answer + random.randint(-20, 20)
         if fake_answer != answer and fake_answer not in choices:
             choices.append(fake_answer)
